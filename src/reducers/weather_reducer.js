13782e6a39031d3fe3eedc070da4c8b73f969e8f
Samuel Lyon <sblyon@me.com>
2016-08-01 12:11:05 -0700

    can overwrite cities

--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -1,9 +1,16 @@
 import { FETCH_WEATHER, RESULTS_RECEIVED } from '../actions/action_types';
 
+const filterState = (state, newCityName) => {
+  return state.filter(weatherObj => {
+    return weatherObj.city.name !== newCityName
+  });
+}
+
 export default function weatherReducer(state = [], action) {
   switch (action.type) {
     case RESULTS_RECEIVED:
-      return [action.payload, ...state]; // used to be action.query
+      return [action.payload,
+        ...filterState(state, action.payload.city.name)];
     default: return state;
   }
 }
